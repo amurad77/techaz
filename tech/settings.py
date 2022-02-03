746de0389fb3f1572975a36f271fcafeed51f4b0
amurad77 <amurad7799@gmail.com>
2022-02-04 00:01:17 +0400

    _

--- tech/settings.py
+++ tech/settings.py
@@ -194,7 +194,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static')
@@ -202,10 +202,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
-# if DEBUG:
-#     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-# else:
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
