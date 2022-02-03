db20bc6380822619f2cb38741e80d1140919512f
amurad77 <amurad7799@gmail.com>
2022-02-04 01:29:02 +0400

    migrationss

--- tech/settings.py
+++ tech/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = ')brx##30=up6zlagvyy_wg64b=ws4y1a$7f7mu5juprp_@anx='
 # DEBUG = False if os.environ.get('DEBUG') else True
 # PROD = not DEBUG
 DEBUG = False
-PROD = True
+PROD = False
 
 ALLOWED_HOSTS = ['*']
 
