fc59e4204af2414e00c22ebceda7df0b282bf835
amurad77 <amurad7799@gmail.com>
2022-02-03 23:11:05 +0400

    fixed

--- tech/settings.py
+++ tech/settings.py
@@ -195,19 +195,19 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 
 
 
 
 
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# if DEBUG:
+#     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+# else:
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
