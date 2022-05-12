bad7741eb9023d6a55e24351217ff5fbc4ef64f5
husubeyli <husubeyli@gmail.com>
2022-05-12 20:25:21 +0400

    last version fixed articles comment

--- articles/api/urls.py
+++ articles/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls import url
 from .routers import router
 from .viewsets import ArticleViewSets, MixDataViewSets, AllData
@@ -13,14 +13,14 @@ urlpatterns = [
 
     path('alldata/', AllData.as_view()),
     path('mixdata/', MixDataViewSets.as_view()),
-    url(r'articles/(?P<pk>\d+)/', view=ArticlesDetailAPIView.as_view()),
-    path('articles/(?P<pk>)/comments/', view=ArticleViewSets.as_view({'get':'comments', 'post':'comments'})),
-    path('articles/(?P<pk>)/comments/(?P<comment>\d+)/', view=ArticleViewSets.as_view({'delete':'remove_comment'})),
-    url(r'articles/(?P<pk>\d+)/comments/$', view=ArticleViewSets.as_view({'get':'comments', 'post':'comments'})),
-    url(r'articles/(?P<pk>\d+)/comments/(?P<comment_id>\d+)/$', view=ArticleViewSets.as_view({'delete':'remove_comment'})),
-    url(r'articles-comments-reply/(?P<pk>\d+)/comments/(?P<comment_id>\d+)/$', view=ArticleViewSets.as_view({'get':'reply_comment', 'post':'reply_comment', 'delete': 'remove_comment'})),
-    url(r'articles-comments-remove/(?P<pk>\d+)/comments/(?P<comment_id>\d+)/$', view=ArticleViewSets.as_view({'delete':'remove_comment'})),
-    url(r'articles-comments/(?P<pk>\d+)/comments/(?P<comment_id>\d+)/$', view=ArticleViewSets.as_view({'get':'reply_comment', 'post': 'reply_comment'})),
+    re_path(r'^articles/(?P<pk>\d+)/$', view=ArticlesDetailAPIView.as_view()),
+    re_path(r'^articles/(?P<pk>)/comments/$', view=ArticleViewSets.as_view({'get':'comments', 'post':'comments'})),
+    re_path(r'^articles/(?P<pk>)/comments/(?P<comment>\d+)/$', view=ArticleViewSets.as_view({'delete':'remove_comment'})),
+    # re_path(r'^articles/(?P<pk>\d+)/comments/$', view=ArticleViewSets.as_view({'get':'comments', 'post':'comments'})),
+    # re_path(r'^articles/(?P<pk>\d+)/comments/(?P<comment_id>\d+)/$', view=ArticleViewSets.as_view({'delete':'remove_comment'})),
+    re_path(r'^articles-comments-reply/(?P<pk>\d+)/comments/(?P<comment_id>\d+)/$', view=ArticleViewSets.as_view({'get':'reply_comment', 'post':'reply_comment', 'delete': 'remove_comment'})),
+    re_path(r'^articles-comments-remove/(?P<pk>\d+)/comments/(?P<comment_id>\d+)/$', view=ArticleViewSets.as_view({'delete':'remove_comment'})),
+    re_path(r'^articles-comments/(?P<pk>\d+)/comments/(?P<comment_id>\d+)/$', view=ArticleViewSets.as_view({'get':'reply_comment', 'post': 'reply_comment'})),
     
 ]
 
--- tech/settings.py
+++ tech/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = ')brx##30=up6zlagvyy_wg64b=ws4y1a$7f7mu5juprp_@anx='
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False if os.environ.get('DEBUG') else True
 # PROD = not DEBUG
-DEBUG = False
-PROD = True
+DEBUG = True
+PROD = not DEBUG
 
 ALLOWED_HOSTS = ['*']
 
