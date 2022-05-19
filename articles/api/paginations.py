cc4e803da8139d008d5fcdf027892aa3a94a7cc7
amurad77 <amurad7799@gmail.com>
2022-05-19 10:49:50 -0700

    fixed mix data pagination

--- articles/api/paginations.py
+++ articles/api/paginations.py
@@ -3,7 +3,7 @@ from drf_multiple_model.pagination import MultipleModelLimitOffsetPagination
 
 
 class LimitPagination(MultipleModelLimitOffsetPagination):
-    default_limit = 4
+    default_limit = 10
 
 class LimitPaginationArticles(MultipleModelLimitOffsetPagination):
     default_limit = 9
