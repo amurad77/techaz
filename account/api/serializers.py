be582f1e86dba06f32b1fcf880274b4198761ad1
amurad77 <amurad7799@gmail.com>
2022-06-06 16:04:19 +0400

    delete register some params

--- account/api/serializers.py
+++ account/api/serializers.py
@@ -18,10 +18,10 @@ class UserSerializer(serializers.ModelSerializer):
             'last_name',
             'email',
             'username',
-            'education',
-            'position',
+            # 'education',
+            # 'position',
             'image',
-            'birthday',
+            # 'birthday',
             'token',
             'articles'
         ]
@@ -55,8 +55,19 @@ class UserSerializerCreate(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password2', 'first_name', 'last_name', 'education', 'position', 'birthday',
-                'image', 'created_at', 'updated_at',]
+        fields = ['username', 
+        'email', 
+        'password', 
+        'password2', 
+        'first_name', 
+        'last_name', 
+        # 'education', 
+        # 'position', 
+        # 'birthday',
+        'image', 
+        'created_at', 
+        'updated_at'
+        ]
         extra_kwargs = {
             'password': {'write_only': True},
             'first_name': {'required': True},
@@ -68,9 +79,9 @@ class UserSerializerCreate(serializers.ModelSerializer):
     def create(self, validated_data):
         first_name = validated_data.get('first_name', '')
         last_name = validated_data.get('last_name', '')
-        position = validated_data.get('position', '')
-        education = validated_data.get('education', '')
-        birthday = validated_data.get('birthday', '')
+        # position = validated_data.get('position', '')
+        # education = validated_data.get('education', '')
+        # birthday = validated_data.get('birthday', '')
         image = validated_data.get('image', '')
         username = validated_data.get('username', '')
         email = validated_data.get('email', '')
@@ -87,8 +98,16 @@ class UserSerializerCreate(serializers.ModelSerializer):
         
         if password != password2:
             raise serializers.ValidationError({'password': 'The two passwords differ.'})
-        user = User(username=username, email=email, first_name=first_name, last_name=last_name,
-                    position=position, image=image, education=education, birthday=birthday)
+        user = User(
+        username=username, 
+        email=email, 
+        first_name=first_name, 
+        last_name=last_name,
+        # position=position, 
+        image=image, 
+        # education=education, 
+        # birthday=birthday
+        )
         user.set_password(password)
         user.save()
         return user
@@ -106,3 +125,12 @@ class ProfileUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'education', 'position', 'image','birthday',]
+
+
+
+
+
+
+
+
+
